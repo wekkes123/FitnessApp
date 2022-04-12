64e5a9fccc70029cc44695abef683846700291ae
LAPTOP-PKRHRB5J\vanlo <vanloo.wout@gmail.com>
2022-04-12 14:04:21 +0200

    update

--- app/src/main/java/be/kuleuven/fitnessapp/Login.java
+++ app/src/main/java/be/kuleuven/fitnessapp/Login.java
@@ -1,9 +1,11 @@
 package be.kuleuven.fitnessapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Login extends AppCompatActivity {
 
@@ -12,6 +14,12 @@ public class Login extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //zorcht voor fullscreen
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //end
         setContentView(R.layout.activity_main);
 
     }
