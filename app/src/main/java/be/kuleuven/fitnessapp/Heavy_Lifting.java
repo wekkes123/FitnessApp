882d997f3170d9ce621de7e0dfbaf954722a94fe
tobiaszTS <100778890+tobiaszTS@users.noreply.github.com>
2022-04-12 14:22:24 +0200

    all categories done

---
+++ app/src/main/java/be/kuleuven/fitnessapp/Heavy_Lifting.java
@@ -0,0 +1,14 @@
+package be.kuleuven.fitnessapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Heavy_Lifting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_heavy_lifting);
+    }
+}
