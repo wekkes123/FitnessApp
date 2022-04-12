7adf24d72e4384e5deb1c6ec87ca2ed23795016f
tobiaszTS <100778890+tobiaszTS@users.noreply.github.com>
2022-04-12 13:50:01 +0200

    choose screen, done

---
+++ app/src/main/java/be/kuleuven/fitnessapp/Cardio.java
@@ -0,0 +1,14 @@
+package be.kuleuven.fitnessapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Cardio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cardio);
+    }
+}
