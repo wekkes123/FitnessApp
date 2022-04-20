836c34cf3585792be31b69aba90e63de32847c1d
LAPTOP-PKRHRB5J\vanlo <vanloo.wout@gmail.com>
2022-04-20 16:37:05 +0200

    update

--- app/src/main/java/be/kuleuven/fitnessapp/classes/ActivityC/Login.java
+++ app/src/main/java/be/kuleuven/fitnessapp/classes/ActivityC/Login.java
@@ -55,9 +55,7 @@ public class Login extends AppCompatActivity {
     }
 
     public void onLoginButtonClicked(View caller){
-        //nog niet klaar
-        requestLogin();
-        LoginAction logindata = new LoginAction(Username.getText().toString(), Password.getText().toString());
+        LoginAction logindata = new LoginAction(Username.getText().toString(), Password.getText().toString(), this);
         if(logindata.checkLoginInfo()){
             Intent intent = new Intent(this, Categories.class);
             startActivity(intent);
@@ -71,30 +69,6 @@ public class Login extends AppCompatActivity {
     public void onDebugClicked(View caller){
         Intent debug = new Intent(this, Categories.class);
         startActivity(debug);
-    }
-
-    public void requestLogin(){
-        RequestQueue requestqueue = Volley.newRequestQueue(this);
-        String requestURL = "https://studev.groept.be/api/a21pt213/SelectAll";
-
-        StringRequest submitRequest = new StringRequest(Request.Method.GET, requestURL,
-
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        System.out.print(response);
-                    }
-                },
-
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        System.out.print("error");
-                    }
-                }
-        );
-
-        requestqueue.add(submitRequest);
     }
 
 }
--- app/src/main/java/be/kuleuven/fitnessapp/classes/Models/LoginAction.java
+++ app/src/main/java/be/kuleuven/fitnessapp/classes/Models/LoginAction.java
@@ -1,5 +1,6 @@
 package be.kuleuven.fitnessapp.classes.Models;
 
+import android.content.Context;
 import android.os.Parcel;
 import android.os.Parcelable;
 import android.view.View;
@@ -15,19 +16,27 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.common.hash.Hashing;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 
 import be.kuleuven.fitnessapp.classes.ActivityC.Login;
 
 public class LoginAction {
     private String Username;
     private String Password;
+    private Context LoginContext;
     private RequestQueue requestqueue;
 
 
-    public LoginAction(String username, String password){
+    public LoginAction(String username, String password, Context context){
         Username = username;
         Password = encryptPassword(password);
+        LoginContext = context;
+        requestLogin();
     }
 
     public String encryptPassword(String pw){
@@ -39,10 +48,40 @@ public class LoginAction {
     public boolean checkLoginInfo(){
         return false;
     }
+    public void requestLogin(){
+        RequestQueue requestqueue = Volley.newRequestQueue(LoginContext);
+        String requestURL = "https://studev.groept.be/api/a21pt213/SelectAll";
 
+        StringRequest submitRequest = new StringRequest(Request.Method.GET, requestURL,
 
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONArray responseArray = new JSONArray(response);
+                            ArrayList<String> data = new ArrayList<String>();
+                            for(int i = 0; i < responseArray.length(); i++){
+                                JSONObject curObject = responseArray.getJSONObject(i);
+                                data.add(curObject.getString("Username"));
+                                data.add(curObject.getString("Password"));
+                            }
+                            System.out.println(data.get(0) + data.get(1) + data.get(2) + data.get(3));
+                        }
+                        catch( JSONException e ){
+                            //display error message
+                        }
+                    }
+                },
 
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        System.out.print("error");
+                    }
+                }
+        );
 
-
+        requestqueue.add(submitRequest);
+    }
 
 }
