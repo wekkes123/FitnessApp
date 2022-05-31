b5bec2f70afbacc616259138be929d1027849c86
tobiaszTS <100778890+tobiaszTS@users.noreply.github.com>
2022-05-31 10:18:37 +0200

    random changes

--- app/src/main/java/be/kuleuven/fitnessapp/classes/ActivityC/Exercise.java
+++ app/src/main/java/be/kuleuven/fitnessapp/classes/ActivityC/Exercise.java
@@ -40,7 +40,6 @@ public class Exercise extends AppCompatActivity implements
     int[] IntNumbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
 
 
-
     public String title;
     private TextView tv1;
     private TextView titleText;
